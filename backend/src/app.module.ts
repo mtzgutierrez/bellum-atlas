b40c53a0b557029199087ee86e10c16b5c587f7c
Martinez Gutierrez Raul <rmg429@inlumine.ual.es>
2026-05-02 15:51:40 +0200

    feat: update backend dependencies and configure environment variables
    - Added @nestjs/config and pg dependencies for environment variable management and PostgreSQL support.
    - Updated Prisma configuration to use prisma-client-js.
    - Integrated ConfigModule in AppModule for global environment variable access.
    - Loaded environment variables in main.ts using dotenv.
    - Refactored PrismaService to remove direct database connection handling.
    
    chore: add devcontainer configuration for frontend development
    
    - Created a devcontainer.json file for setting up a development environment with VSCode extensions and settings.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(__dirname, '..', '.env'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,25 +1,8 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor() {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
-
-    const adapter = new PrismaPg(pool);
-
-    super({ adapter });
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     await this.$connect();
